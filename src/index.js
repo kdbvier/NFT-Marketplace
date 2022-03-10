5e311811320cacdf437b8a32a3e2d9f2feba13c7
tanvir-crastonic <tanvir.hossain@crastonic.com>
2022-03-11 01:07:51 +0600

    Redux tool kit added for global store

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import "./index.css";
 import "./assets/css/Header.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />,
+    <Provider store={store}>
+      <App />,
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
