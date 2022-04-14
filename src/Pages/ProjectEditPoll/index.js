2842052c7627a1fdd911467faf692e161b1797e5
tanvir-crastonic <tanvir.hossain@crastonic.com>
2022-04-15 04:43:53 +0600

    CW2F-145, Poll: Money transfer UI

--- src/Pages/ProjectEditPoll/index.js
+++ src/Pages/ProjectEditPoll/index.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import ProjectEditTopNavigationCard from "components/ProjectEdit/ProjectEditTopNavigationCard";
 import ProjectCover from "components/ProjectEdit/ProjectCover";
 import { getProjectPollList } from "services/project/projectService";
-import Moment from "moment";
+import PollListView from "components/ProjectEdit/Poll/PollList";
+import CreatePoll from "components/ProjectEdit/Poll/CreatePoll";
 
 function ProjectPoll(props) {
   const [isLoading, setIsLoading] = useState(false);
   const [pollList, setPollList] = useState([]);
   const [total, setTotal] = useState(1);
+  const [showNewPost, setShowNewPost] = useState(false);
 
   const projectId = props.match.params.id;
   let pageNo = 1;
@@ -33,6 +35,10 @@ function ProjectPoll(props) {
     getPollList(projectId);
   }
 
+  function closeNewPost() {
+    setShowNewPost(false);
+  }
+
   return (
     <>
       <div className="sticky z-[1]">
@@ -44,69 +50,25 @@ function ProjectPoll(props) {
       <div className={`grid justify-items-center`}>
         <span className="text-5xl font-bold">POLL</span>
         <div className="mt-8 divide-y divide-gray-300 w-full sm:w-2/3 px-4 sm:mx-0">
-          <div className="text-center sm:text-right">
-            <span className="inline-flex items-center justify-center px-3.5 py-1.5 text-lg text-white font-bold leading-none bg-[#0AB4AF] rounded-full mb-4">
-              New Post
-            </span>
-          </div>
-          {pollList && pollList.length > 0 ? (
-            <>
-              {pollList.map((poll, index) => (
-                <div className="pt-5" key={`poll-list-${index}`}>
-                  <div className="flex">
-                    <span
-                      className={`inline-flex items-center justify-center px-3.5 py-1.5 text-sm sm:text-s leading-none border ${
-                        poll.status === "success"
-                          ? "border-[#0AB4AF]"
-                          : poll.status === "failed"
-                          ? "border-red-400"
-                          : "border-gray-400"
-                      } rounded-full`}
-                    >
-                      <span className="font-medium">DUE DATE&nbsp;&nbsp;</span>{" "}
-                      {Moment(poll.start_at).format("YYYY/MM/DD")} -
-                      {Moment(poll.expire_at).format("YYYY/MM/DD")}
-                    </span>
-                    <span className="hidden sm:block ml-4 mt-2">
-                      {Moment(poll.updated_at).format("YYYY/MM/DD")}
-                    </span>
-                  </div>
-                  <p className="block sm:hidden mt-1.5">
-                    {Moment(poll.updated_at).format("YYYY/MM/DD")}
-                  </p>
-                  <div className="text-base sm:text-lg font-bold mt-1">
-                    {poll.title}
-                  </div>
-                  <div className="relative bottom-12 text-right text-gray-400">
-                    <i className="fa fa-angle-right fa-lg sm:fa-2x" />
-                  </div>
-                </div>
-              ))}
-            </>
-          ) : (
-            <></>
-          )}
-          {isLoading ? (
-            <div className={`h-12 w-full text-center`}>
-              <i
-                class="fa fa-spinner fa-spin fa-3x text-[#0AB4AF] mt-4 "
-                aria-hidden="true"
-              ></i>
-            </div>
+          {showNewPost === true ? (
+            <CreatePoll closeNewPost={closeNewPost} />
           ) : (
             <>
-              {total - pageSize > 0 ? (
-                <div className={`h-12 w-full text-center pt-8`}>
-                  <span
-                    className="text-[#0AB4AF] cursor-pointer font-medium"
-                    onClick={getMore}
-                  >
-                    See more
-                  </span>
-                </div>
-              ) : (
-                <></>
-              )}
+              <div className="text-center sm:text-right">
+                <span
+                  onClick={() => setShowNewPost(true)}
+                  className="inline-flex items-center justify-center px-3.5 py-1.5 text-lg text-white font-bold leading-none bg-[#0AB4AF] rounded-full mb-4"
+                >
+                  New Post
+                </span>
+              </div>
+              <PollListView
+                pollList={pollList}
+                isLoading={isLoading}
+                total={total}
+                pageSize={pageSize}
+                getMore={getMore}
+              />
             </>
           )}
         </div>
