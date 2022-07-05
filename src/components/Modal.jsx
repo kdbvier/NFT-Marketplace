ec3b6f73f63783666f49ad6d51599223bd93938a
Md. Sahab Uddin <shihab@Mds-MacBook-Pro.local>
2022-07-05 10:17:32 +0600

    ui for minit nft

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,14 +21,15 @@ const Modal = ({ handleClose, show, children, height, width }) => {
       <section
         onClick={(e) => modalBodyClicked(e)}
         style={styleObj}
-        className={" modal-main bg-[#161423] rounded-3xl"}
+        className={" modal-main bg-dark-background rounded-3xl relative text-white p-11"}
       >
-        <img
+        {/* <img
           alt=""
           src={ModalCloseLogo}
           onClick={handleClose}
           className="cursor-pointer ml-auto mt-[26px] mr-[26px] h-[20px] w-[20px]"
-        />
+        /> */}
+        <i class="fa-light fa-square-xmark cursor-pointer text-2xl absolute top-12 right-8" onClick={handleClose}></i>
         <div className="">{children}</div>
       </section>
     </div>
