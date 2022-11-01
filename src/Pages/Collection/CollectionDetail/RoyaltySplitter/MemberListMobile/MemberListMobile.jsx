2fd583d28125984ab670a8c5ce8cd3b1e3fb878d
trietda <33666307+trietda@users.noreply.github.com>
2022-11-01 11:58:04 +0700

    feat: mobile update splitter percentage (#371)
    Co-authored-by: jeyanthCrastonic <109852544+jeyanthCrastonic@users.noreply.github.com>

--- src/Pages/Collection/CollectionDetail/RoyaltySplitter/MemberListMobile/MemberListMobile.jsx
+++ src/Pages/Collection/CollectionDetail/RoyaltySplitter/MemberListMobile/MemberListMobile.jsx
@@ -1,71 +1,130 @@
+import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import Trash from "assets/images/icons/trash.svg";
+import Edit from "assets/images/icons/edit.svg";
 import { walletAddressTruncate } from "util/WalletUtils";
 
-const MemeberListMobile = ({ list }) => {
+const MemberRowMobile = (props) => {
+  const {
+    item,
+    isLastItem,
+    handleValueChange,
+    handleAutoFill,
+  } = props;
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleUpdatePercent = async (e) => {
+    await handleAutoFill(e);
+    setIsEdit(false);
+  };
+
+  return (
+    <
+      div className={`${isLastItem ? "border-b" : ""} pb-4 mb-4`}>
+      <div className="flex items-center justify-between">
+        <div className="mb-3">
+          <p className="text-[14px] font-bold">Wallet Address</p>
+          <div className="flex items-center">
+            <p className="text-[13px] mt-0">
+              {walletAddressTruncate(item.user_eoa)}
+            </p>
+            <CopyToClipboard text={item.user_eoa}>
+              <button className="ml-1 w-[32px] h-[32px] rounded-[4px] flex items-center justify-center cursor-pointer text-[#A3D7EF] active:text-black">
+                <FontAwesomeIcon className="" icon={faCopy} />
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        {item.is_owner ? null : (
+          <div className="w-[32px] h-[32px] bg-[#FF3C3C] rounded-[4px] flex items-center justify-center cursor-pointer">
+            <img src={Trash} alt="delete" />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center">
+        <div className="w-2/6">
+          <p className="text-[14px] font-bold">Name</p>
+          <p className="text-[13px] mt-0">
+            {item.user_name ? item.user_name : "-"}
+          </p>
+        </div>
+        <div className="w-2/6">
+          <p className="text-[14px] font-bold">Percentage</p>
+          <div className="flex">
+            {!isEdit && (
+              <>
+                <span className="text-[13px] mt-0">
+                  {item.royalty_percent ? `${item.royalty_percent}%` : "-"}
+                </span>
+                <img
+                  className="ml-2 cursor-pointer"
+                  src={Edit}
+                  alt="edit"
+                  onClick={() => setIsEdit(true)}
+                />
+              </>
+            )}
+            {isEdit && (
+              <div className="flex">
+                <div className="w-[60px]">
+                  <input
+                    type="number"
+                    value={item.royalty_percent}
+                    style={{ padding: "5px 10px" }}
+                    onChange={(e) => handleValueChange(e, item.user_eoa)}
+                  />
+                </div>
+                <div className="ml-1">
+                  <i
+                    class="fa-solid fa-check bg-green-400 rounded-[4px] text-white flex items-center justify-center h-[24px] w-[24px] text-[20px] cursor-pointer"
+                    onClick={handleUpdatePercent}
+                  ></i>
+                  <i
+                    class="fa-solid fa-xmark bg-red-400 rounded-[4px] text-white flex items-center justify-center h-[24px] w-[24px] text-[20px] cursor-pointer"
+                    onClick={() => setIsEdit(false)}
+                  ></i>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="w-2/6">
+          <p className="text-[14px] font-bold">Roles</p>
+          <p
+            className={`text-[13px] mt-0 bg-opacity-[0.2] py-1 px-2 w-fit rounded-[4px] font-bold ${
+              item.is_owner
+                ? "text-info-1 bg-[#46A6FF]"
+                : " text-success-1 bg-[#32E865]"
+            }`}
+          >
+            {item.is_owner ? "Owner" : "Contributor"}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const MemeberListMobile = (props) => {
+  const {
+    list,
+    handleAutoFill,
+    handleValueChange,
+  } = props;
+
   return (
     <div>
       {list?.length &&
         list.map((item, index) => (
-          <div
+          <MemberRowMobile
             key={index}
-            className={`${index < list.length - 1 ? "border-b" : ""} pb-4 mb-4`}
-          >
-            <div className="flex items-center justify-between">
-              <div className="mb-3">
-                <p className="text-[14px] font-bold">Wallet Address</p>
-                <div className="flex items-center">
-                  <p className="text-[13px] mt-0">
-                    {walletAddressTruncate(item.user_eoa)}
-                  </p>
-                  <CopyToClipboard text={item.user_eoa}>
-                    <button className="ml-1 w-[32px] h-[32px] rounded-[4px] flex items-center justify-center cursor-pointer text-[#A3D7EF] active:text-black">
-                      <FontAwesomeIcon className="" icon={faCopy} />
-                    </button>
-                  </CopyToClipboard>
-                </div>
-              </div>
-              {item.is_owner ? null : (
-                <div className="w-[32px] h-[32px] bg-[#FF3C3C] rounded-[4px] flex items-center justify-center cursor-pointer">
-                  <img src={Trash} alt="delete" />
-                </div>
-              )}
-            </div>
-            <div className="flex items-center">
-              <div className="w-2/6">
-                <p className="text-[14px] font-bold">Name</p>
-                <p className="text-[13px] mt-0">
-                  {item.user_name ? item.user_name : "-"}
-                </p>
-              </div>
-              <div className="w-2/6">
-                <p className="text-[14px] font-bold">Percentage</p>
-                <p className="text-[13px] mt-0">
-                  {item.royalty_percent ? `${item.royalty_percent}%` : "-"}
-                </p>
-              </div>
-              {/* <div className="w-2/6">
-              <p className="text-[14px] font-bold">Token ID</p>
-              <p className="text-[13px] mt-0">
-                {item.token_id ? item.token_id : "-"}
-              </p>
-            </div> */}
-              <div className="w-2/6">
-                <p className="text-[14px] font-bold">Roles</p>
-                <p
-                  className={`text-[13px] mt-0 bg-opacity-[0.2] py-1 px-2 w-fit rounded-[4px] font-bold ${
-                    item.is_owner
-                      ? "text-info-1 bg-[#46A6FF]"
-                      : " text-success-1 bg-[#32E865]"
-                  }`}
-                >
-                  {item.is_owner ? "Owner" : "Contributor"}
-                </p>
-              </div>
-            </div>
-          </div>
+            item={item}
+            isLastItem={index === list.length - 1}
+            handleAutoFill={handleAutoFill}
+            handleValueChange={handleValueChange}
+          />
         ))}
     </div>
   );
