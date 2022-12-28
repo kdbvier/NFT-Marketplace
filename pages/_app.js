576306c75f40239803dcb9c9af69e9cfff10271d
tanzilCn <96968733+tanzilCn@users.noreply.github.com>
2022-12-28 22:44:11 +0600

    added maintainence mode (#546)

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,9 @@ import MetaHead from 'components/Commons/MetaHead/MetaHead';
 import Head from 'next/head';
 import Favicon from 'components/Commons/Favicon';
 dynamic(() => import('tw-elements'), { ssr: false });
+import axios from 'axios';
+import Config from 'config/config';
+import Maintenance from 'components/Commons/Maintenance';
 
 export const persistor = persistStore(store);
 
@@ -26,21 +29,34 @@ function MyApp({ Component, pageProps }) {
   const [showSideBar, setShowSideBar] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [isEmbedView, setIsEmbedView] = useState(false);
+  const [isMaintenance, setIsMaintenance] = useState(false);
   const router = useRouter();
   const handleToggleSideBar = () => {
     setShowSideBar(!showSideBar);
   };
 
   const { data } = pageProps;
+  const getMaintenanceInfo = async () => {
+    await axios
+      .get(`${Config.MAINTENANCE_MODE_URL}/status.json`)
+      .then((res) => {
+        if (res?.maintain) {
+          setIsMaintenance(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
+    getMaintenanceInfo();
     let path = typeof window !== 'undefined' && router?.asPath;
     let pathItems = path && path.split('/');
     let view =
       pathItems && pathItems.length ? pathItems.includes('embed-nft') : false;
     setIsEmbedView(view);
   }, [router?.asPath]);
-
 
   return (
     <>
@@ -59,56 +75,60 @@ function MyApp({ Component, pageProps }) {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <DAppProvider config={{}}>
-            <Auth>
-              <div className='bg-light'>
-                {!isEmbedView && (
-                  <Header
-                    handleSidebar={handleToggleSideBar}
-                    setShowModal={setShowModal}
-                    showModal={showModal}
-                  />
-                )}
-                <main className='container min-h-[calc(100vh-71px)]'>
-                  <div className='flex flex-row'>
-                    {!isEmbedView && (
-                      <div className='hidden md:block mr-4 '>
-                        <Sidebar
-                          setShowModal={setShowModal}
-                          handleToggleSideBar={handleToggleSideBar}
-                        />
-                      </div>
-                    )}
-                    {!isEmbedView && (
-                      <div
-                        className={`${
-                          showSideBar ? 'translate-x-0' : '-translate-x-full'
-                        } block md:hidden mr-4 absolute z-[100] ease-in-out duration-300`}
-                      >
-                        <Sidebar
-                          setShowModal={setShowModal}
-                          handleToggleSideBar={handleToggleSideBar}
-                        />
-                      </div>
-                    )}
-                    <div className='w-full min-w-[calc(100vw-300px)]'>
-                      <Component {...pageProps} />
+            {isMaintenance ? (
+              <Maintenance />
+            ) : (
+              <Auth>
+                <div className='bg-light'>
+                  {!isEmbedView && (
+                    <Header
+                      handleSidebar={handleToggleSideBar}
+                      setShowModal={setShowModal}
+                      showModal={showModal}
+                    />
+                  )}
+                  <main className='container min-h-[calc(100vh-71px)]'>
+                    <div className='flex flex-row'>
+                      {!isEmbedView && (
+                        <div className='hidden md:block mr-4 '>
+                          <Sidebar
+                            setShowModal={setShowModal}
+                            handleToggleSideBar={handleToggleSideBar}
+                          />
+                        </div>
+                      )}
+                      {!isEmbedView && (
+                        <div
+                          className={`${
+                            showSideBar ? 'translate-x-0' : '-translate-x-full'
+                          } block md:hidden mr-4 absolute z-[100] ease-in-out duration-300`}
+                        >
+                          <Sidebar
+                            setShowModal={setShowModal}
+                            handleToggleSideBar={handleToggleSideBar}
+                          />
+                        </div>
+                      )}
+                      <div className='w-full min-w-[calc(100vw-300px)]'>
+                        <Component {...pageProps} />
 
-                      <ToastContainer
-                        className='impct-toast'
-                        position='top-right'
-                        autoClose={3000}
-                        newestOnTop
-                        closeOnClick
-                        rtl={false}
-                        pauseOnVisibilityChange
-                        draggable={false}
-                        transition={Slide}
-                      />
+                        <ToastContainer
+                          className='impct-toast'
+                          position='top-right'
+                          autoClose={3000}
+                          newestOnTop
+                          closeOnClick
+                          rtl={false}
+                          pauseOnVisibilityChange
+                          draggable={false}
+                          transition={Slide}
+                        />
+                      </div>
                     </div>
-                  </div>
-                </main>
-              </div>
-            </Auth>
+                  </main>
+                </div>
+              </Auth>
+            )}
           </DAppProvider>
         </PersistGate>
       </Provider>
