3bfcfed232be41862450cacd0625323163cc9ee1
jeyanthCrastonic <109852544+jeyanthCrastonic@users.noreply.github.com>
2023-03-01 21:32:41 +0530

    Fixed royalty claim issue (#694)
    Co-authored-by: Jeyanth Kanagaraj <jeyanthkanagaraj@gmail.com>

--- components/Profile/TransactionDetails/TransactionDetailsContent.jsx
+++ components/Profile/TransactionDetails/TransactionDetailsContent.jsx
@@ -13,6 +13,10 @@ import WithdrawModal from 'components/Collection/WithdrawModal';
 import NetworkHandlerModal from 'components/Modals/NetworkHandlerModal';
 import { toast } from 'react-toastify';
 import Link from 'next/link';
+import LeavingSite from 'components/Project/Components/LeavingSite';
+import ErrorModal from 'components/Modals/ErrorModal';
+import { royaltyClaim } from '../royalty-claim';
+import { createProvider } from 'util/smartcontract/provider';
 
 const ITEMS = {
   dao: {
@@ -69,6 +73,12 @@ export default function TransactionDetailsContent({ query }) {
   const [showNetworkHandler, setShowNetworkHandler] = useState(false);
   const [collection, setCollection] = useState();
   const [totalValue, setTotalValue] = useState(0);
+  const [selectedDAO, setSelectedDAO] = useState(null);
+  const [showDAOClaim, setShowDAOClaim] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [royaltyErrormessage, setRoyaltyErrormessage] = useState('');
+  const [errorModal, setErrorModal] = useState(false);
+  const provider = createProvider();
 
   useEffect(() => {
     setSelectedTab(query?.tab);
@@ -95,6 +105,11 @@ export default function TransactionDetailsContent({ query }) {
     handleSetValues();
   }, [royaltiesList, collectionList]);
 
+  const handleDAOClaim = (data) => {
+    setSelectedDAO(data);
+    setShowDAOClaim(true);
+  };
+
   async function getProjectList() {
     let payload = {
       id: userinfo?.id,
@@ -114,28 +129,12 @@ export default function TransactionDetailsContent({ query }) {
     const payload = {
       id: userinfo?.id,
       page: 1,
-      limit: 5,
+      limit: 10,
     };
     await getUserCollections(payload)
       .then((e) => {
         if (e.code === 0 && e.data !== null) {
-          console.log(e.data);
           setCollectionList(e.data);
-          // let total = e?.data?.reduce(
-          //   (acc, net) => acc + net?.summary?.holding_value_usd,
-          //   0
-          // );
-
-          // let totalValues = values.map((value) => {
-          //   if (value?.key === 'collection') {
-          //     return {
-          //       ...value,
-          //       value: total,
-          //     };
-          //   }
-          //   return value;
-          // });
-          // setValues(totalValues);
         }
       })
       .catch(() => {});
@@ -183,20 +182,6 @@ export default function TransactionDetailsContent({ query }) {
             element.isLoading = false;
           });
           setRoyaltiesList(res.royalties);
-          // let total = res.royalties?.reduce(
-          //   (acc, net) => acc + net?.earnable_amount,
-          //   0
-          // );
-          // let totalValues = values.map((value) => {
-          //   if (value?.key === 'royalty') {
-          //     return {
-          //       ...value,
-          //       value: total,
-          //     };
-          //   }
-          //   return value;
-          // });
-          // setValues(totalValues);
           if (res.total && res.total > 0) {
             const page = calculatePageCount(5, res.total);
             const pageList = [];
@@ -260,21 +245,20 @@ export default function TransactionDetailsContent({ query }) {
       };
       let config = {};
       let hasConfig = false;
+      setIsLoading(true);
       await claimRoyalty(payload)
         .then((res) => {
           if (res.code === 0) {
             config = res.config;
             hasConfig = true;
           } else {
-            // setIsLoading(false);
-            // setErrorModal(true);
-            // setRoyaltyErrormessage(res.message);
+            setErrorModal(true);
+            setRoyaltyErrormessage(res.message);
             setRoyaltyData(royalty, 'loadingFalse');
           }
         })
         .catch(() => {
-          // setIsLoading(false);
-          // setErrorModal(true);
+          setErrorModal(true);
         });
       if (hasConfig) {
         const result = await royaltyClaim(provider, config);
@@ -305,7 +289,7 @@ export default function TransactionDetailsContent({ query }) {
       setShowNetworkHandler(true);
     }
   }
-
+  console.log(royaltiesList);
   return (
     <div className='bg-[#e2ecf0] pt-[30px] md:min-h-[100vh]'>
       {showNetworkHandler && (
@@ -324,7 +308,26 @@ export default function TransactionDetailsContent({ query }) {
           price={collection?.summary?.holding_value}
           contractAddress={collection?.contract_address}
           type={collection?.type}
-          getCollectionNewWorth={() => {}}
+          getCollectionNewWorth={getCollectionList}
+        />
+      )}
+      {showDAOClaim && (
+        <LeavingSite
+          network={selectedDAO?.blockchain}
+          treasuryAddress={selectedDAO?.treasury_wallet}
+          show={showDAOClaim}
+          handleClose={() => setShowDAOClaim(false)}
+        />
+      )}
+      {errorModal && (
+        <ErrorModal
+          title={'Royalty can not claim right now.'}
+          royaltyErrormessage={royaltyErrormessage}
+          handleClose={() => {
+            setErrorModal(false);
+            setRoyaltyErrormessage('');
+          }}
+          show={errorModal}
         />
       )}
       <div className='w-full px-4 pb-10 md:max-w-[1200px] mx-auto'>
@@ -426,7 +429,11 @@ export default function TransactionDetailsContent({ query }) {
                                 </td>
                                 <td class='whitespace-nowrap px-6 py-4'>-</td>
                                 <td class='whitespace-nowrap px-6 py-4'>
-                                  <button className='text-[#2AD100] text-[12px] font-bold border-[#2AD100] border-[1px] rounded-[8px] h-[32px] w-[88px]'>
+                                  <button
+                                    disabled={list?.status !== 'published'}
+                                    onClick={() => handleDAOClaim(list)}
+                                    className='text-[#2AD100] text-[12px] font-bold border-[#2AD100] border-[1px] rounded-[8px] h-[32px] w-[88px]'
+                                  >
                                     Claim
                                   </button>
                                 </td>
@@ -502,7 +509,9 @@ export default function TransactionDetailsContent({ query }) {
                                 </td>
                                 <td class='whitespace-nowrap px-6 py-4'>
                                   <button
-                                    disabled={!list?.withdraw_value}
+                                    disabled={
+                                      list?.summary?.holding_value === 0
+                                    }
                                     onClick={(e) =>
                                       handleWithdrawModel(e, list)
                                     }
@@ -586,13 +595,39 @@ export default function TransactionDetailsContent({ query }) {
                                   {list?.earnable_amount} Token
                                 </td>
                                 <td class='whitespace-nowrap px-6 py-4'>
-                                  <button
-                                    disabled={!list?.list?.earnable_amount}
-                                    onClick={() => claimRoyaltyById(list)}
-                                    className='text-[#2AD100] text-[12px] font-bold border-[#2AD100] border-[1px] rounded-[8px] h-[32px] w-[88px]'
-                                  >
-                                    Claim
-                                  </button>
+                                  {list.isLoading ? (
+                                    <div role='status' className=''>
+                                      <svg
+                                        aria-hidden='true'
+                                        className=' w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-secondary-900'
+                                        viewBox='0 0 100 101'
+                                        fill='none'
+                                        xmlns='http://www.w3.org/2000/svg'
+                                      >
+                                        <path
+                                          d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                                          fill='currentColor'
+                                        />
+                                        <path
+                                          d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                                          fill='currentFill'
+                                        />
+                                      </svg>
+                                      <span className='sr-only'>
+                                        Loading...
+                                      </span>
+                                    </div>
+                                  ) : (
+                                    <>
+                                      <button
+                                        disabled={!list?.list?.earnable_amount}
+                                        onClick={() => claimRoyaltyById(list)}
+                                        className='text-[#2AD100] text-[12px] font-bold border-[#2AD100] border-[1px] rounded-[8px] h-[32px] w-[88px]'
+                                      >
+                                        Claim
+                                      </button>
+                                    </>
+                                  )}
                                 </td>
                               </tr>
                             ))
