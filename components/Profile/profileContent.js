e35abc3132cda08a3b634315a5ec0d5e64b6aa18
jeyanthCrastonic <109852544+jeyanthCrastonic@users.noreply.github.com>
2023-03-13 09:56:15 +0530

    update use case images and link (#708)
    Co-authored-by: NGUYEN TIEN LONG <tienlongbk.k51@gmail.com>
    Co-authored-by: tanzilCn <tanzil.islam@crastonic.com>

--- components/Profile/profileContent.js
+++ components/Profile/profileContent.js
@@ -27,6 +27,13 @@ import Image from 'next/image';
 import CreateNFTModal from 'components/Project/CreateDAOandNFT/components/CreateNFTModal.jsx';
 import emptyStateCommon from 'assets/images/profile/emptyStateCommon.svg';
 import Modal from 'components/Commons/Modal';
+import charity from 'assets/images/profile/charity.png';
+import dao from 'assets/images/profile/dao.png';
+import fashion from 'assets/images/profile/fashion.png';
+import invest from 'assets/images/profile/invest.png';
+import membership from 'assets/images/profile/membership.png';
+import pfp from 'assets/images/profile/pfp.png';
+
 import {
   createTokenGatedProject,
   getTokenGatedProjectList,
@@ -56,16 +63,22 @@ const nftUseCase = {
       title: 'Membership NFT',
       description:
         'Offer personalized experiences through token-gated NFTs. Offer membership, design rewards, and create categories-based access services',
+      img: membership,
+      url: 'https://decir.io/decir-users-to-web3-asset-owners/',
     },
     {
       title: 'PFP',
       description:
         'PFP NFT helps your brand build and engage your online community. It is a great tool for brand identity',
+      img: pfp,
+      url: 'https://decir.io/what-are-pfp-nfts-used-for/',
     },
     {
       title: 'Digital Fashion',
       description:
         'Design the next set of virtual fashion collectibles tailored to the increasing demands of the virtual world',
+      img: fashion,
+      url: 'https://decir.io/introduction-of-nft-in-digital-fashion/',
     },
   ],
 };
@@ -77,16 +90,22 @@ const daoUseCase = {
       title: 'Build',
       description:
         'Build web3 projects to create diverse income streams for your members through the power of the DAO',
+      img: dao,
+      url: 'https://decir.io/decir-no-code-dao-tool/',
     },
     {
       title: 'Charity',
       description:
         'Create a DAO that caters to the needs of communities around the world. Support noble courses through the power of the collective.',
+      img: charity,
+      url: 'https://decir.io/decir-token-gated-dao-communities/',
     },
     {
       title: 'Invest',
       description:
         'Invest in web3 startups and in physical assets through a DAO. Forge shared prosperity through mutual benefits',
+      img: invest,
+      url: 'https://decir.io/decir-for-web3-project-fundraising/',
     },
   ],
 };
