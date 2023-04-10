cedbc3ad47380f7029427012829b5a6b535159df
Jeyanth Kanagaraj <jeyanthkanagaraj@gmail.com>
2023-04-10 11:28:40 +0530

    Added fix for redux persist

--- redux/index.js
+++ redux/index.js
@@ -1,8 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 import { persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
 
 const persistConfig = {
   key: 'root',
